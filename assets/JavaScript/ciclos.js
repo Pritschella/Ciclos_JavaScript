a6e0876e5a70f61cc83f40a0da6e2f0eff722632
Pritschella <prits99@hotmail.com>
2023-09-07 12:09:33 -0600

    Se agregan apuntes hechos en clase acerca de ciclos

---
+++ assets/JavaScript/ciclos.js
@@ -0,0 +1,12 @@
+/*
+
+Condiciones: alterar el orden natural del codigo
+estructura de control: controlar la ejecución del código
+
+
+Ciclos o bucles  (loops)
+
+También conocidos como bucles, son estructuras de control que nos van a permitir ejecutar un bloque de código de forma repetida, hasta que se cumpla una condición específica.
+
+
+*/
